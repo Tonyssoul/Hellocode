9fd625e0884cc4bbd3da97c73d6179191d09ff9a
Anton Kargapolov <igloooooook@gmail.com>
2023-01-31 21:43:17 +0500

    exp 8

---
+++ Example_010_Methodarray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 11, 21, 32, 43, 86, 52, 63, 74, 86 };
+
+int n = array.Length;
+int find = 86;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
--- example001_helloConsole/obj/Debug/net6.0/example001_helloConsole.AssemblyInfo.cs
+++ example001_helloConsole/obj/Debug/net6.0/example001_helloConsole.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("example001_helloConsole")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("example001_helloConsole")]
-[assembly: System.Reflection.AssemblyTitleAttribute("example001_helloConsole")]
+[assembly: System.Reflection.AssemblyProductAttribute("Example001_HelloConsole")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
