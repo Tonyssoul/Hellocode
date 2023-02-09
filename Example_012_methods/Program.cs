43641b5b6156a1b51ec5bfc0a9732c1b70d6cbd6
Anton Kargapolov <igloooooook@gmail.com>
2023-02-09 18:05:53 +0500

    methods to 012 last

--- Example_012_methods/Program.cs
+++ Example_012_methods/Program.cs
@@ -91,10 +91,89 @@ string res = Method4(10, "asdf");
 // System.Console.WriteLine(res);
 
 
-for (int i = 2; i <= 10; i++)
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         System.Console.WriteLine($"{i} x {j}={i * j}");
+//     }
+// }
+
+
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+//  маленькие буквы “к” заменить большими “К”, 
+//  а большие “С” заменить маленькими “с”.
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо вашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Выдадите мне чаю?";
+
+
+//string s = "qwerty"
+//             012
+// s[3] // r
+
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int lenght = text.Length;
+//     for (int i = 0; i < lenght; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+// newText = Replace(text, 'к', 'К');
+// System.Console.WriteLine(newText);
+
+
+
+// 1. Найти позицию минимального элемента в неотсортированной части массива
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции 
+// 3. Повторять пока есть не отсортированные элементы
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
 {
-    for (int j = 2; j <= 10; j++)
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
     {
-        System.Console.WriteLine($"{i} x {j}={i * j}");
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
     }
 }
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
